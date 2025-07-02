0a285335a630b54837b98a3e0b36839b871d5c62
Tulio Faria <tuliofaria@gmail.com>
2025-07-02 17:48:50 -0300

    avoid adding empty change

--- admin/src/app/changes.tsx
+++ admin/src/app/changes.tsx
@@ -31,7 +31,7 @@ export const Changes = ({ fields, ids }: Props) => {
 
     const addToBatch = () => {
 
-        if (productId === '' || selectedField === '') {
+        if (productId === '' || selectedField === '' || value === '') {
             return
         }
 
